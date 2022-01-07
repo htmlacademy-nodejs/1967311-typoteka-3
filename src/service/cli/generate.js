ef2d29e66ceea44eb45494b7ccff49f902d75774
ruslanamirov <ruslanamiroff@gmail.com>
2022-01-07 23:04:42 +0300

    Личный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,43 +8,9 @@ const MIN_OFFER_SENTENCE_COUNT = 1;
 const MAX_OFFER_SENTENCE_COUNT = 5;
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-  'Продам коллекцию журналов «Огонёк».',
-  'Отдам в хорошие руки подшивку «Мурзилка».',
-  'Продам советскую посуду. Почти не разбита.',
-  'Куплю детские санки.'
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  'Кажется, что это хрупкая вещь.',
-  'Мой дед не мог её сломать.',
-  'Кому нужен этот новый телефон, если тут такое...',
-  'Не пытайтесь торговаться. Цену вещам я знаю.'
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+const FILE_SENTENCES_PATH = `../../data/sentences.txt`;
+const FILE_TITLES_PATH = `../../data/titles.txt`;
+const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
 
 const OfferType = {
   OFFER: `offer`,
@@ -62,26 +28,41 @@ const PictureRestrict = {
   MAX: 16,
 };
 
-const generateOffers = (count ) => (
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences ) => (
   Array(count || DEFAULT_COUNT).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(MIN_OFFER_SENTENCE_COUNT, MAX_OFFER_SENTENCE_COUNT).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(MIN_OFFER_SENTENCE_COUNT, MAX_OFFER_SENTENCE_COUNT).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
   }))
 );
 
+
 const generate = {
   name: '--generate',
     async run(params) {
+      const sentences = await readContent(FILE_SENTENCES_PATH);
+      const titles = await readContent(FILE_TITLES_PATH);
+      const categories = await readContent(FILE_CATEGORIES_PATH);
+
       const count = +params[0] || DEFAULT_COUNT;
       if (count > MAX_OFFER_COUNT) {
         return console.error(chalk.red('Не больше 1000 объявлений'))
       }
       try {
-        await fs.writeFile(FILE_NAME, JSON.stringify(generateOffers(count)))
+        await fs.writeFile(FILE_NAME, JSON.stringify(generateOffers(count, titles, categories, sentences)))
       } catch (err) {
         console.error('generation error: ', err);
         process.exit(1)
