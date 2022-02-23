b0901c114c36968e0f67afe9d7cc8a14798bb037
ruslanamirov <ruslanamiroff@gmail.com>
2022-02-23 22:00:40 +0300

    module3-task3: Фиксы

--- src/service/service.js
+++ src/service/service.js
@@ -4,7 +4,6 @@ const {Cli} = require(`./cli`);
 const {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  EXIT_CODE
 } = require(`../constants`);
 
 const userArguments = process.argv.slice(USER_ARGV_INDEX);
@@ -12,7 +11,7 @@ const [userCommand] = userArguments;
 
 if (userArguments.length === 0 || !Cli[userCommand]) {
   Cli[DEFAULT_COMMAND].run();
-  process.exit(EXIT_CODE);
+  process.exit();
 }
 
 Cli[userCommand].run(userArguments.slice(1));
